c526d03942a539a1da22ff745140b65f0b5128dc
2643358332 <124060465+2643358332@users.noreply.github.com>
2023-11-14 13:27:01 +0000

    🎉 INT COMMIT: 即刻修改 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/13/hello-world/"];function toRandomPost(){
+var posts=["2023/11/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
